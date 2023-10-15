2874462ada82df569bf2086fbdb17840c7aac709
h00ns <jihoon.hwang@imweb.me>
2023-10-15 10:09:13 +0900

    [feat] google oauth layout

--- src/pages/Login/Form/index.css.ts
+++ src/pages/Login/Form/index.css.ts
@@ -1,6 +1,8 @@
+import { c_gap_1, flx_center } from '@/style/display.css';
 import { style } from '@vanilla-extract/css';
+import { Radius, gray, white } from 'hoon-ds';
 
-export const text_button = style({
+export const text_btn = style({
   fontSize: '14px',
   lineHeight: '20px',
   cursor: 'pointer',
@@ -9,3 +11,23 @@ export const text_button = style({
     opacity: 0.7,
   },
 });
+
+export const google_btn = style([
+  c_gap_1,
+  flx_center,
+  {
+    padding: '9px 12px',
+    fontSize: '14px',
+    lineHeight: '20px',
+    borderRadius: Radius.MEDIUM,
+    color: gray.gray6,
+    cursor: 'pointer',
+
+    border: 'none',
+    background: white,
+
+    ':hover': {
+      opacity: 0.7,
+    },
+  },
+]);
--- src/pages/Login/Form/index.tsx
+++ src/pages/Login/Form/index.tsx
@@ -2,10 +2,11 @@ import { flx_between, r_gap_1 } from '@/style/display.css';
 import { mt_1, mt_2 } from '@/style/margin.css';
 import { Button, Input } from 'hoon-ds';
 import { useState } from 'react';
-import { text_button } from './index.css';
+import { google_btn, text_btn } from './index.css';
 import { useNavigate } from 'react-router-dom';
 import { HOME, RESET, SIGN_UP } from '@/constants/routes/routes';
 import { useLoginFetch } from '@/hooks/fetch/useAuthFetch';
+import GoogleImg from '@/assets/images/google.png';
 
 type LoginFormType = {
   email: string;
@@ -66,11 +67,15 @@ export default function Form() {
         onChange={handleFormChange}
       />
       <Button className={mt_1} text="로그인하기" />
+      <button className={google_btn} type="button">
+        <img width={16} height={16} src={GoogleImg} alt="google" />
+        Google로 로그인
+      </button>
       <div className={flx_between}>
-        <span className={text_button} onClick={() => navigate(RESET)}>
+        <span className={text_btn} onClick={() => navigate(RESET)}>
           비밀번호 찾기
         </span>
-        <span className={text_button} onClick={() => navigate(SIGN_UP)}>
+        <span className={text_btn} onClick={() => navigate(SIGN_UP)}>
           회원가입
         </span>
       </div>
