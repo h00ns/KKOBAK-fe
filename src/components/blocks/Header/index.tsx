64e2bea230a55d898f5fe14f76d897204dc9c052
h00ns <jihoon.hwang@imweb.me>
2023-11-07 20:56:00 +0900

    [feat] myPage setting

--- src/components/blocks/Header/index.tsx
+++ src/components/blocks/Header/index.tsx
@@ -1,36 +1,47 @@
 import { Icon, white } from 'hoon-ds';
-import { date_text, graph_icon, header, icon_wrapper } from './index.css';
+import { date_text, header, icon_wrap, menu_icon, profile } from './index.css';
 import { useNavigate } from 'react-router-dom';
 import { useDateActions, useYearMonthState } from '@/store/date';
-import { Toast } from '@/utils/toast';
+import { MY_PAGE, STATISTICS } from '@/constants/routes/routes';
 
-export default function Header() {
+type Props = {
+  pageType: 'home' | 'statistics';
+};
+
+export default function Header({ pageType }: Props) {
   const navigate = useNavigate();
 
   const { year, month } = useYearMonthState();
   const { handlePrevMonth, handleNextMonth } = useDateActions();
 
-  const handleLogout = () => {
-    localStorage.removeItem('accessToken');
-    localStorage.removeItem('refreshToken');
-    navigate('/');
-    Toast.success('로그아웃 되었습니다.');
-  };
-
   return (
-    <div className={`${header}`}>
-      <div className={graph_icon} onClick={handleLogout}>
-        <Icon name="logout" fill="transparent" stroke={white} />
-      </div>
-      <div className={icon_wrapper} onClick={handlePrevMonth}>
+    <div className={header}>
+      {/* only 홈 (통계 페이지로) */}
+      {pageType === 'home' && (
+        <div className={menu_icon} onClick={() => navigate(STATISTICS)}>
+          <Icon name="menu" fill={white} stroke={white} />
+        </div>
+      )}
+      {/* only 홈 end */}
+
+      {/* only 통계 페이지 (뒤로가기) */}
+      {pageType === 'statistics' && (
+        <div className={menu_icon} onClick={() => navigate(-1)}>
+          <Icon name="chevron-left" stroke={white} />
+        </div>
+      )}
+      {/* only 통계 페이지 end */}
+
+      <div className={icon_wrap} onClick={handlePrevMonth}>
         <Icon name="chevron-left" stroke={white} />
       </div>
       <div className={date_text}>
         {year}.{month}
       </div>
-      <div className={icon_wrapper} onClick={handleNextMonth}>
+      <div className={icon_wrap} onClick={handleNextMonth}>
         <Icon name="chevron-right" stroke={white} />
       </div>
+      <div className={profile} onClick={() => navigate(MY_PAGE)}></div>
     </div>
   );
 }
---
+++ src/pages/mypage/index.tsx
@@ -0,0 +1,38 @@
+import { withAuth } from '@/components/hocs/withAuth';
+import { Toast } from '@/utils/toast';
+import { Icon, TypoVariant, Typography, white } from 'hoon-ds';
+import { useNavigate } from 'react-router-dom';
+import { back_icon, header, logout_icon, profile, profile_img_wrap } from './index.css';
+
+const MyPage = () => {
+  const navigate = useNavigate();
+
+  /**
+   *  로그아웃
+   */
+  const handleLogout = () => {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    navigate('/');
+    Toast.success('로그아웃 되었습니다.');
+  };
+
+  return (
+    <div className={header}>
+      <div className={back_icon} onClick={() => navigate(-1)}>
+        <Icon name="chevron-left" fill="transparent" stroke={white} />
+      </div>
+      <div className={profile}>
+        <div className={profile_img_wrap}></div>
+        <Typography variant={TypoVariant.B2} color={white}>
+          skjo666@gmail.com
+        </Typography>
+      </div>
+      <div className={logout_icon} onClick={handleLogout}>
+        <Icon name="logout" fill="transparent" stroke={white} />
+      </div>
+    </div>
+  );
+};
+
+export default withAuth(MyPage);
